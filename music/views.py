7fad2b0bfe495734b07b98b8a3f5a1feb4f3c1d7
Alhrafas <abbadebugger@gmail.com>
2024-02-06 13:19:00 +0100

    still makings in my view for authentiacting and signing new user

--- music/views.py
+++ music/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from .models import Artist
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 
 # Create your views here.
@@ -36,17 +37,31 @@ def profile(request):
 
 def signup(request):
     if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
 
-        if password1 == password2:  # Check if passwords match
-             print('password same')
-             
+        if password == password2:  # Check if passwords match
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already taken")
+                return redirect("signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "username already taken")
+                return redirect("signup")
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
+                user.save()
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect("/")
+
         else:
-             messages.info(request, "password not matching")
-             return redirect('singup')   
+            messages.info(request, "password not matching")
+            return redirect("signup")
 
     else:
         return render(request, "signup.html")
