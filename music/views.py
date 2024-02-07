085ea8c1c57c2ee9947253b58a39c84f0fc98b12
Alhrafas <abbadebugger@gmail.com>
2024-02-07 12:57:24 +0100

    using api for top artsits in views

--- music/views.py
+++ music/views.py
@@ -3,25 +3,58 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+import requests
 
-@login_required(login_url='login')
+
+def top_artists():
+    url = "https://spotify-scraper.p.rapidapi.com/v1/chart/artists/top"
+
+    headers = {
+        "X-RapidAPI-Key": "f7836c33e5msh7b72a9ccc12699dp1aa833jsn2338b1c30d2d",
+        "X-RapidAPI-Host": "spotify-scraper.p.rapidapi.com",
+    }
+
+    response = requests.get(url, headers=headers)
+    response_data = response.json()
+
+    artists_info = []
+
+    if "artists" in response_data:
+        for artist in response_data["artists"]:
+            name = artist.get("name", "No Name")
+            avatar_url = artist.get("visuals", {}).get("avatar", [{}])[0].get("url", 'No URL')
+            artist_id = artist.get("id", "No ID")
+            artists_info.append((name, avatar_url, artist_id))
+            
+            return artists_info   
+
+
+@login_required(login_url="login")
 def index(request):
-    return render(request, "index.html")
+    artists_info = top_artists()
+    print(artists_info)
+    context = {
+         'artists_info': artists_info,
+    }
+    return render(request, "index.html", context)
+
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return redirect("index")
         else:
-            messages.error(request, 'Invalid username or password.')
+            messages.error(request, "Invalid username or password.")
     return render(request, "login.html")
+
 
 def profile(request):
     return render(request, "profile.html")
+
 
 def signup(request):
     if request.method == "POST":
@@ -49,7 +82,8 @@ def signup(request):
 
     return render(request, "signup.html")
 
-@login_required(login_url='login')
+
+@login_required(login_url="login")
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect("login")
