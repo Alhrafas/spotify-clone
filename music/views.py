2332c9aecb8bc95c361685ca18819e9061d60f5a
Alhrafas <abbadebugger@gmail.com>
2024-02-15 14:14:59 +0100

    changes in music

--- music/views.py
+++ music/views.py
@@ -26,11 +26,12 @@ def get_top_songs():
 def play_song(request, song_id):
     # Retrieve the song object from the database
     song = get_object_or_404(Song, pk=song_id)
+    artist = song.album.artist
 
     # Additional logic for playing the song can be added here
 
     # Pass the song object to the template
-    return render(request, 'music.html', {'song': song})
+    return render(request, 'music.html', {'song': song, 'artist': artist})
 
 @login_required(login_url="login")
 def index(request):
